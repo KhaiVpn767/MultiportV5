4a13d6af3d10e93e23752b56b55cd8f2cae7f020
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2025-06-24 15:26:55 +0800

    Update updatefile.sh

--- updatefile.sh
+++ updatefile.sh
@@ -4,28 +4,50 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 echo -e " [INFO] Downloading Update File"
 sleep 2
+# hapus menu
+rm -rf ins-helium
+rm -rf bbr
+rm -rf wssgen
+rm -rf add-host
+rm -rf speedtest
+rm -rf xp
+rm -rf menu
+rm -rf status
+rm -rf info
+rm -rf restart
+rm -rf ram
+rm -rf dns
+rm -rf nf
+rm -rf limit
+rm -rf menu-tr
+rm -rf menu-ws
+rm -rf menu-vless
+rm -rf menu-xtr
+rm -rf certxray
+
+
 # download script
 cd /usr/local/sbin
-wget -O ins-helium "https://${Server_URL}/ins-helium.sh"
-wget -O bbr "https://${Server_URL}/bbr.sh"
-wget -O wssgen "https://${Server_URL}/wssgen.sh"
-wget -O add-host "https://${Server_URL}/add-host.sh"
-wget -O speedtest "https://${Server_URL}/speedtest_cli.py"
-wget -O xp "https://${Server_URL}/xp.sh"
-wget -O menu "https://${Server_URL}/menu.sh"
-wget -O status "https://${Server_URL}/status.sh"
-wget -O info "https://${Server_URL}/info.sh"
-wget -O restart "https://${Server_URL}/restart.sh"
-wget -O ram "https://${Server_URL}/ram.sh"
-wget -O dns "https://${Server_URL}/dns.sh"
-wget -O nf "https://${Server_URL}/media.sh"
-wget -O limit "https://${Server_URL}/limit-speed.sh"
-wget -O menu-tr "https://${Server_URL}/menu-tr.sh"
-wget -O menu-ws "https://${Server_URL}/menu-ws.sh"
-wget -O menu-vless "https://${Server_URL}/menu-vless.sh"
-wget -O menu-xtr "https://${Server_URL}/menu-xtr.sh"
-wget -O menu-xrt "https://${Server_URL}/menu-xrt.sh"
-wget -O certxray "https://${Server_URL}/cert.sh"
+wget -O https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/main/ins-helium.sh"
+wget -O bbr "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/bbr.sh"
+wget -O wssgen "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/wssgen.sh"
+wget -O add-host "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/add-host.sh"
+wget -O speedtest "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/speedtest_cli.py"
+wget -O xp "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/xp.sh"
+wget -O menu "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/menu.sh"
+wget -O status "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/status.sh"
+wget -O info "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/info.sh"
+wget -O restart "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/restart.sh"
+wget -O ram "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/ram.sh"
+wget -O dns "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/dns.sh"
+wget -O nf "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/media.sh"
+wget -O limit "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/limit-speed.sh"
+wget -O menu-tr "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/menu-tr.sh"
+wget -O menu-ws "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/menu-ws.sh"
+wget -O menu-vless "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/menu-vless.sh"
+wget -O menu-xtr "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/menu-xtr.sh"
+wget -O menu-xrt "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/menu-xrt.sh"
+wget -O certxray "https://raw.githubusercontent.com/KhaiVpn767/MultiportV5/cert.sh"
 chmod +x menu-tr
 chmod +x menu-ws
 chmod +x menu-vless
